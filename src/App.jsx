111c24621f6b699de64c7177a93afa12d5f4b6c1
murarigupta <murarigupta28598@gmail.com>
2025-01-21 11:11:58 +0530

    Setting up LMS main frontend part with all the required dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return(
+    <div className='font-bold text-3xl bg-black text-white text-center mt-11'>
+      Lms main 
+    </div>
+  )
+}
+
+export default App
